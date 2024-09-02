a7a017548132e5a6b059ee83038213a2ac464d7a
M02laleague <9558@holbertonstudents.com>
2024-09-02 12:15:28 +0200

    Task 0

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,8 +5,21 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1
+        n -= 1  # On décrémente n pour éviter la boucle infinie
     return result
 
-f = factorial(int(sys.argv[1]))
-print(f)
+if __name__ == "__main__":
+    # Vérifie que l'utilisateur a passé un argument
+    if len(sys.argv) > 1:
+        try:
+            # Convertit l'argument en entier
+            n = int(sys.argv[1])
+            if n >= 0:
+                f = factorial(n)
+                print(f)
+            else:
+                print("Veuillez entrer un entier positif.")
+        except ValueError:
+            print("Veuillez entrer un nombre entier valide.")
+    else:
+        print("Veuillez fournir un nombre en argument.")
